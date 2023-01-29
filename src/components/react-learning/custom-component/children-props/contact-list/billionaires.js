def4af6f01db37ca98b52f26d1625b9cfe1d704a
0rigin-c0de <shunnu807@gmail.com>
2023-01-29 11:02:12 +0530

    initial-commit

---
+++ src/components/react-learning/custom-component/children-props/contact-list/billionaires.js
@@ -0,0 +1,16 @@
+export let billionaires = [
+  {
+    name: "Bill Gates",
+    email: "billg@microsoft.com",
+    photoURL: "https://frontarm.com/courses/react-fundamentals/bill-gates.jpg",
+  },
+  {
+    name: "Jeff Bezos",
+    email: "jeff@amazon.com",
+    photoURL: "https://frontarm.com/courses/react-fundamentals/jeff-bezos.jpg",
+  },
+  {
+    name: "Mark Zuckerberg",
+    email: "zuck@fb.com",
+  },
+];
