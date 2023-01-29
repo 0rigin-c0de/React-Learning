aea1dbab29645c6a13f9a877f319b74429c67dca
0rigin-c0de <shunnu807@gmail.com>
2023-01-29 10:41:36 +0530

    initial-commit

---
+++ src/components/Component-State/use-state/syntax.js
@@ -0,0 +1,7 @@
+let state = useState(1);
+let name = state[0];
+let setName = state[1];
+
+// useState() is called.
+// It returns an array.
+// Its two values get put into the name and setName variables.
